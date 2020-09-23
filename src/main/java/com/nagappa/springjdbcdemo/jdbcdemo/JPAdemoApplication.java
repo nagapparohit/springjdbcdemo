c9587f1c74ca70b8a84cd88d64eb0ba4ec5edc07
rohit nagappa <rnagappa35@gmail.com>
2020-09-23 21:20:29 +0530

    implemented merge method for both update and insert

--- src/main/java/com/nagappa/springjdbcdemo/jdbcdemo/JPAdemoApplication.java
+++ src/main/java/com/nagappa/springjdbcdemo/jdbcdemo/JPAdemoApplication.java
@@ -1,5 +1,7 @@
 package com.nagappa.springjdbcdemo.jdbcdemo;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nagappa.springjdbcdemo.jdbcdemo.entity.Person;
 import com.nagappa.springjdbcdemo.jdbcdemo.jpa.PersonJPARepo;
 /**
  * This will automatically create becuase hibernate have schema update feautre as spring sees entity ,jpa in classpath
@@ -28,17 +31,26 @@ public class JPAdemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("\nUser id 10001 -->{}",repository.findById(10001));
+		
+		/*
+		 * logger.info("\ninserting 10004 -->No of rows changed--> {}",repository.insert
+		 * (new Person(10004,"Tara","Berlin", new Date())));
+		 */
+		//hibernate will ignore the id if its new record so best way to do is remove id from above cmd.
+		logger.info("\ninserting 10004 -->No of rows changed--> {}",repository.insert(new
+				  Person("Tara","Berlin", new Date())));
+		logger.info("\nupdate 10003 --> No of rows changed-->{}",repository.update(new
+				  Person(10003,"peter","Delhi", new Date())));
+				 
+				 
+		
 		/*
 		 * logger.info("\nAll users --> {}",dao.findAll());
 		 * logger.info("\ndeleting id 10002 --> No of rows deleted-->{}",dao.deleteById(
 		 * 10002)); logger.info("\nAfter Deleting All users --> {}",dao.findAll());
-		 * logger.info("\ninserting 10004 -->No of rows changed--> {}",dao.insert(new
-		 * Person(10004,"Tara","Berlin", new Date())));
-		 */
-		/*
-		 * logger.info("\nupdate 10003 --> No of rows changed-->{}",dao.update(new
-		 * Person(10003,"peter","Delhi", new Date())));
-		 */
+		 */	  
+		
+		 
 		
 		
 	}
--- src/main/java/com/nagappa/springjdbcdemo/jdbcdemo/jpa/PersonJPARepo.java
+++ src/main/java/com/nagappa/springjdbcdemo/jdbcdemo/jpa/PersonJPARepo.java
@@ -25,4 +25,21 @@ public class PersonJPARepo {
 		return entityManager.find(Person.class,id);//jpa
 	
 	}
+	
+	/**
+	 * Whether you want to update or insert you need to call the merge method
+	 * merge will knw if there is id set in the person then it will update
+	 * and if there is no id set it will insert the person.
+	 * @param person
+	 * @return
+	 */
+	public Person update(Person person) {
+		return entityManager.merge(person);
+	}
+	
+	
+	public Person insert(Person person) {
+		return entityManager.merge(person);
+	}
+	
 }
